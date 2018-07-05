1fbde52a0c9c0d459b01967d2d057eb5daa73e82
Evan Freeze <evanfreeze@gmail.com>
2018-07-05 16:33:15 -0500

    Work we did on 7/5

--- src/App.js
+++ src/App.js
@@ -1,20 +1,61 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import fetch from 'node-fetch';
+
+function Character(props) {
+  const { id, name, image, gender } = props
+  return (
+    <div key={id} className="character">
+      <h1>{name}</h1>
+      <img src={image} alt={name}/>
+      <h2>{gender}</h2>
+    </div>
+  )
+}
+
+function Button(props) {
+  return <button onClick={() => props.clickFunction(props.buttonText)}>{props.buttonText.toUpperCase()}</button>
+}
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      isLoaded: false,
+      characters: [],
+      statusToShow: 'Alive',
+    }
+    this.updateStatus = this.updateStatus.bind(this)
+  }
+
+  componentDidMount() {
+    fetch('https://rickandmortyapi.com/api/character/')
+      .then(response => response.json())
+      .then(data => this.setState({
+        characters: data.results,
+        isLoaded: true,
+      }))
+  }
+
+  updateStatus(statusToShow) {
+    this.setState({ statusToShow })
+  }
+
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+    const aliveCharacters = this.state.characters.filter(character => character.status === this.state.statusToShow)
+    console.log(aliveCharacters)
+    if (this.state.isLoaded) {
+      return (
+        <div className="App">
+          <Button clickFunction={this.updateStatus} buttonText="Dead" />
+          <Button clickFunction={this.updateStatus} buttonText="Alive" />
+          <Button clickFunction={this.updateStatus} buttonText="unknown" />
+          {aliveCharacters.map(character => <Character {...character} key={character.id} />)}
+        </div>
+      );
+    } else {
+      return <h2>Loading...</h2>
+    }
   }
 }
 
