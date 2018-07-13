0c0c46b3c3570fa2ddd45009b4680bdf387be467
Evan Freeze <evanfreeze@gmail.com>
2018-07-13 14:26:40 -0500

    renames Character to Card, removes buttons

--- src/App.js
+++ src/App.js
@@ -2,15 +2,15 @@ import React, { Component } from 'react';
 import './App.css';
 import fetch from 'node-fetch';
 
-function Character(props) {
+function Card(props) {
   const { id, name, image, gender, species } = props
   return (
-    <div key={id} className="character">
-      <div className="character-header">
+    <div key={id} className="card">
+      <div className="card-header">
         <img src={image} alt={name}/>
         <h1>{name}</h1>
       </div>
-      <div className="character-details">
+      <div className="card-details">
         <h2>Gender</h2>
         <h2>{gender}</h2>
         <h2>Species</h2>
@@ -18,37 +18,6 @@ function Character(props) {
       </div>
     </div>
   )
-}
-
-/* 
-
-  {
-    "id": 183,
-    "name": "Johnny Depp",
-    "status": "Alive",
-    "species": "Human",
-    "type": "",
-    "gender": "Male",
-    "origin": {
-      "name": "Earth (C-500A)",
-      "url": "https://rickandmortyapi.com/api/location/23"
-    },
-    "location": {
-      "name": "Earth (C-500A)",
-      "url": "https://rickandmortyapi.com/api/location/23"
-    },
-    "image": "https://rickandmortyapi.com/api/character/avatar/183.jpeg",
-    "episode": [
-      "https://rickandmortyapi.com/api/episode/8"
-    ],
-    "url": "https://rickandmortyapi.com/api/character/183",
-    "created": "2017-12-29T18:51:29.693Z"
-  }
-
-  */
-
-function Button(props) {
-  return <button onClick={() => props.clickFunction(props.buttonText)}>{props.buttonText.toUpperCase()}</button>
 }
 
 class App extends Component {
@@ -81,11 +50,8 @@ class App extends Component {
     if (this.state.isLoaded) {
       return (
         <div className="App">
-          <Button clickFunction={this.updateStatus} buttonText="Dead" />
-          <Button clickFunction={this.updateStatus} buttonText="Alive" />
-          <Button clickFunction={this.updateStatus} buttonText="unknown" />
-          <div className="characters">
-            {aliveCharacters.map(character => <Character {...character} key={character.id} />)}
+          <div className="cards">
+            {aliveCharacters.map(character => <Card {...character} key={character.id} />)}
           </div>
         </div>
       );
